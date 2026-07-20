5c0b566eac8a56ff9999847ac14662918c4a881c
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-20 15:10:30 +0530

    Add files via upload

---
+++ PROG8.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main()
+{
+    int a[20], i, j, temp, n;
+
+    printf("\nEnter Limit:");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter Value %d:", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - 1 - i; j++)
+        {
+            if(a[j] > a[j + 1])
+            {
+
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nThe elements are in ascending Order:");
+    for(i = 0; i < n; i++)
+    {
+        printf("\n%d", a[i]);
+    }
+}
